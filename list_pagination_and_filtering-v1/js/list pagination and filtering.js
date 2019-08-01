380037ee148d6ae63ec8fffbb57a171c70fe8ed1
amurr <39475006+amurr@users.noreply.github.com>
2019-08-01 01:55:48 -0500

    Add files via upload
    I'm trying to create the showPage function and my console doesn't tell me I have any errors, but I still haven't found a way to get ten list students on each page.

---
+++ list_pagination_and_filtering-v1/js/list pagination and filtering.js
@@ -0,0 +1,97 @@
+/******************************************
+Treehouse Techdegree:
+FSJS project 2 - List Filter and Pagination
+******************************************/
+   
+// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+
+
+/*** 
+   Add your global variables that store the DOM elements you will 
+   need to reference and/or manipulate. 
+   
+   But be mindful of which variables should be global and which 
+   should be locally scoped to one of the two main functions you're 
+   going to create. A good general rule of thumb is if the variable 
+   will only be used inside of a function, then it can be locally 
+   scoped to that function.
+***/
+const listItem = document.querySelectorAll('.student-item');
+const perPage = 10;
+
+
+
+
+
+
+/*** 
+   Create the `showPage` function to hide all of the items in the 
+   list except for the ten you want to show.
+
+   Pro Tips: 
+     - Keep in mind that with a list of 54 students, the last page 
+       will only display four.
+     - Remember that the first student has an index of 0.
+     - Remember that a function `parameter` goes in the parens when 
+       you initially define the function, and it acts as a variable 
+       or a placeholder to represent the actual function `argument` 
+       that will be passed into the parens later when you call or 
+       "invoke" the function 
+***/
+
+const showPage = (list, page) => {
+const startIndex = (page * perPage) - perPage;
+const endIndex = page * perPage;
+
+for(let i = 0; i < listItem.length; i++) {
+
+   if([i] >= startIndex && [i] <= endIndex){
+      listItem[i].style.display = 'block';
+}
+}
+}
+
+
+
+
+/*** 
+   Create the `appendPageLinks function` to generate, append, and add 
+   functionality to the pagination buttons.
+***/
+/*const appendPageLinks = (list) => {
+const pages = listItem / perPage;
+
+const div = document.createElement('div');
+div.setAttribute("class", "pagination");
+
+
+const body = document.getElementsByClassName('page');
+div.append(body);
+
+const ul = document.getElementsByTagName('UL');
+div.append('ul');
+
+const li = document.getElementsByTagName('LI');
+const a = document.getElementsByTagName('A');
+
+for(let i = 0; i < pages.length; i++){
+pages.append('li');
+pages.append('a');
+pages.append('a.textContent');
+
+}
+
+for(let i = 0; i < a.length; i++){
+   a.className = "active";
+   a.classList.remove("active");
+}
+
+a.addEventListener('click', (event) => {
+   a.classList.add("active") = event.target;
+showPage(listItem, 1);
+   
+})
+}
+
+showPage(listItem, 1);
+appendPageLinks(listItem);*/
